d6f59412f51962620a3848223422e35c4aad1072
Caio Costa <caio.c.c.barbosa@gmail.com>
2024-04-12 00:45:41 -0300

    Usuario concluido, inicio do resto

--- login/usuario.py
+++ login/usuario.py
@@ -1,11 +1,8 @@
 class Usuario:
-    def __init__(self, usuario, senha):
-        self.usuario = usuario
-        self.senha = senha
-
-    def cadastrar_usuario(self):
+    
+    def cadastrar_usuario(self, usuario, senha):
         with open('cadastro_usuario.txt', 'a') as cad:
-            cad.write(f'{self.usuario}&&{self.senha}&&\n')
+            cad.write(f'{usuario}&&{senha}&&\n')
 
     
     def visualizar_usuarios(self):
@@ -14,20 +11,19 @@ class Usuario:
             usuarios = cad.readlines()
             for usuario in usuarios:
                 lista.append(usuario.split('&&'))
-
+            
             return lista
 
-    def autenticar_usuario(self, nome, senha):
+    def autenticar_usuario(self, usuario, senha):
         usuarios = self.visualizar_usuarios()
-
-        for usuario in usuarios:
-            if f'{usuario[0]}' == nome and f'{usuario[1]}' == senha:
-                verificar_login = True
-                return 'Logado com sucesso', verificar_login
-            
-            else:
-                verificar_login = False
-                return 'Nome ou senha incorretos', verificar_login
+        
+    
+        
+        for linha in usuarios:
+            if f'{linha[0]}' == usuario and f'{linha[1]}' == senha:
+                return 'Logado com sucesso', True
+                
+        return 'Nome ou senha incorretos', False
 
         
          
--- main.py
+++ main.py
@@ -1,14 +1,71 @@
-from login import usuario
+from login.usuario import Usuario
+import os
 
-while True:
-    nome = input('Digite seu nome para cadastro: ')
-    senha = input('Digite a senha: ')
 
-    controle_usuario = usuario.Usuario(nome, senha)
-    controle_usuario.cadastrar_usuario()
-    print(controle_usuario.visualizar_usuarios())
-    termo1, termo2 = controle_usuario.autenticar_usuario('eduard','321')
-    print(termo1)
-    print(termo2)
+def menu():
+    print('''--------Menu--------
+Digite '1' para entrar na aba de leitor
+Digite '2' para entrar na aba de livro
+Digite '3' para cadastrar um novo login
+Digite '4' para encerrar o programa''')
+
+
+
+def main():
+    controle_usuario = Usuario()
+
+    while True:
+        print('Faça o login')
+        username = input('Digite seu usuario: ')
+        senha = input('Digite sua senha: ')
+        
+        txt, verificacao_login = controle_usuario.autenticar_usuario(username, senha)
+        if verificacao_login == True:
+            os.system('cls')
+            print(txt)
+            break
+        else:
+            os.system('cls')
+            print(txt)
+            continue
+    
+    while True:
+        menu()
+        try:
+            escolha_menu = int(input())
+        except ValueError:
+            os.system('cls')
+            print('Digite um numero!')
+            continue
+
+        match escolha_menu:
+            case 1: # entrar na aba do leitor
+                pass
+
+            case 2: # entrar na aba do livro
+                pass
+
+            case 3: # cadastrar um novo login
+                username = input('Digite o nome do usuario: ')
+                senha = input('Digite sua senha: ')
+                os.system('cls')
+
+                controle_usuario.cadastrar_usuario(username, senha)
+                
+
+            case 4:
+                break
+        
+
+
+
+
+
+    
+
+    
+
+if __name__ == '__main__':
+    main()
 
 
