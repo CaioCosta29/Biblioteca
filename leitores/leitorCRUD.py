cdf5e6bcaf6e147b3a46b995ed08a49a748c3a50
Caio Costa <caio.c.c.barbosa@gmail.com>
2024-04-12 23:58:06 -0300

    Atualizar leitor concluido

---
+++ leitores/leitorCRUD.py
@@ -0,0 +1,49 @@
+class LeitorCRUD:
+    def __init__(self, nome, telefone, email):
+        self.nome = nome.title()
+        self.telefone = telefone
+        self.email = email
+    
+    def cadastrar_leitor(self):
+        with open('cadastro_leitor.txt', 'a') as cad:
+            cad.write(f'{self.nome}&&{self.telefone}&&{self.email}&&\n')
+
+    def cadastrar_leitores(leitores):
+        with open('cadastro_leitor.txt', 'w') as cad:
+            cad.write('')
+
+        for leitor in leitores:
+            LeitorCRUD(leitor[0], leitor[1], leitor[2]).cadastrar_leitor()
+    
+
+    def visualizar_leitores():
+        lista = []
+        with open('cadastro_leitor.txt', 'r') as cad:
+            leitores = cad.readlines()
+
+            for leitor in leitores:
+               lista.append(leitor.split('&&'))
+            
+            return lista
+        
+
+    def atualizar_leitor(self, id_nome, id_telefone):
+        leitores = LeitorCRUD.visualizar_leitores()
+        status_encontrado = False
+
+        for linha in leitores:
+            
+            if f'{id_nome}, {id_telefone}' in f'{linha[0]}, {linha[1]}':
+                status_encontrado = True
+                linha[0] = self.nome
+                linha[1] = self.telefone
+                linha[2] = self.email
+                break
+
+        if status_encontrado == True:
+            LeitorCRUD.cadastrar_leitores(leitores)
+            return 'Leitor atualizado'
+        else:
+            return 'Leitor não encontrado. Impossivel atualizar!'
+            
+            
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from login.usuario import Usuario
+from leitores.leitorCRUD import LeitorCRUD
 import os
 
 
@@ -8,11 +9,14 @@ Digite '1' para entrar na aba de leitor
 Digite '2' para entrar na aba de livro
 Digite '3' para cadastrar um novo login
 Digite '4' para encerrar o programa''')
+    
+
 
 
 
 def main():
     controle_usuario = Usuario()
+    
 
     while True:
         print('Faça o login')
@@ -33,28 +37,68 @@ def main():
         menu()
         try:
             escolha_menu = int(input())
-        except ValueError:
-            os.system('cls')
-            print('Digite um numero!')
-            continue
+    
+            match escolha_menu:
+                case 1: # entrar na aba do leitor
+                    os.system('cls')
+                    print('''----------Leitores----------
+Digite '1' para cadastrar um leitor
+Digite '2' para visualizar leitores
+Digite '3' para editar um leitor
+Digite '4' para excluir um leitor''')
+                    escolha_leitor = int(input())
+                    match escolha_leitor:
+                        case 1:
+                            nome_leitor = input('Digite o nome do leitor: ')
+                            telefone_leitor = input('Digite o telefone: ')
+                            email_leitor = input('Digite o email: ')
+                            LeitorCRUD(nome_leitor, telefone_leitor, email_leitor).cadastrar_leitor()
+                            os.system('cls')
 
-        match escolha_menu:
-            case 1: # entrar na aba do leitor
-                pass
+                        case 2:
+                            if os.path.exists('cadastro_leitor.txt'):
+                                
+                                leitores = LeitorCRUD.visualizar_leitores()
+                                for leitor in leitores:
+                                    print(f'Nome: {leitor[0].ljust(20)} | Telefone: {leitor[1].ljust(20)} | Email: {leitor[2].ljust(20)}')
 
-            case 2: # entrar na aba do livro
-                pass
+                            else:
+                                print('\nNão existe nenhum leitor cadastrado')
+                            
+                            input('Aperte qualquer tecla para continuar')
+                            os.system('cls')
 
-            case 3: # cadastrar um novo login
-                username = input('Digite o nome do usuario: ')
-                senha = input('Digite sua senha: ')
-                os.system('cls')
+                        case 3:
+                            id_nome = input('Digite o nome do leitor que deseja editar: ').title()
+                            id_telefone = input(f'Digite o numero de {id_nome}: ')
 
-                controle_usuario.cadastrar_usuario(username, senha)
+                            nome_atualizado = input('Digite o nome do leitor para atualizar: ')
+                            telefone_atualizado = input('Digite o telefone para atualizar: ')
+                            email_atualizado = input('Digite o email para atualizar: ')
+
+                            txt = LeitorCRUD(nome_atualizado, telefone_atualizado, email_atualizado).atualizar_leitor(id_nome, id_telefone)
+                            os.system('cls')
+                            print(txt)
+                            
+                
+                case 2: # entrar na aba do livro
+                    pass
+
+                case 3: # cadastrar um novo login
+                    username = input('Digite o nome do usuario: ')
+                    senha = input('Digite sua senha: ')
+                    os.system('cls')
+
+                    controle_usuario.cadastrar_usuario(username, senha)
                 
 
-            case 4:
-                break
+                case 4:
+                    break
+
+        except ValueError:
+            os.system('cls')
+            print('Erro, digite um numero valido!')
+
         
 
 
